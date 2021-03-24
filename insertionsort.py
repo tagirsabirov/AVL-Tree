ecb41c2786e357324c1219394aab9131c1506a61
Tagir Sabirov <tagirsabirov1@gmail.com>
2021-03-24 09:56:47 -0700

    Add files via upload

--- insertionsort.py
+++ insertionsort.py
@@ -1,11 +1,25 @@
-#insertion sort using a for loop
-def insertionSort(array):
+def insertionSort(array):    
+    print(f"SORTING {array}")
     for i in range(1, len(array)):
-    #previous number in the array
-        previousnumber = i - 1
-        insertednumber = array[i]
-        while (array[previousnumber] > array[i] and previousnumber >= 0):
-            #moves the inserted number back
-            insertednumber = array[i-1]
+        #previous index in the array     
+        previousindex = i - 1
+        currentvalue = array[i]
+        currentindex = i
+        while (array[currentindex - 1] > currentvalue and (currentindex - 1) >= 0):
+            #swaps the inserted number with the number before it
+            previousnumber = array[currentindex - 1]
+            array[currentindex - 1] = currentvalue
+            array[currentindex] = previousnumber
+            currentindex -= 1   
+    return array
+        
+
+print(insertionSort([1, 3, 2]))
 
 print(insertionSort([1, 2, 3]))
+
+print(insertionSort([6, 5, 4, 3, 2, 1]))
+
+print(insertionSort([3, 2, 1, 5]))
+
+print(insertionSort([2, 3, 1]))
